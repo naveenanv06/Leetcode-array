589658894d1c79c13e67881c99cae4f776b22203
naveenanv06 <naveenanv06@gmail.com>
2026-05-11 22:13:39 +0530

    Time: 3 ms (87.77%) | Memory: 43.3 MB (57.00%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,45 +1,29 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st=new Stack<>();
-        if(s.length()==1){
-            return false;
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            }
+
+            else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+
+                    return false;
+                }
+            }
         }
-        char ch=s.charAt(0);
-        if(ch==')'||ch==']'||ch=='}'){
-                return false;
-            }
-        for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            
-            if(c=='('||c=='['||c=='{'){
-                    st.push(c);
-            }
-            else if(c==')'&&!st.empty()){
-                if(st.peek()=='(')
-                    st.pop();
-                else
-                    st.push(c);
-            }
-            else if(c==']'&&!st.empty()){
-                if(st.peek()=='[')
-                    st.pop();
-                else
-                    st.push(c);
-            }
-            else if(c=='}'&&!st.empty()){
-                if(st.peek()=='{')
-                    st.pop();
-                else
-                    st.push(c);
-            }
-            else
-                st.push(c);
-        }
-        if(st.empty()){
-            return true;
-        }
-        else{
-            return false;
-        }
+
+        return stack.isEmpty();
     }
 }
