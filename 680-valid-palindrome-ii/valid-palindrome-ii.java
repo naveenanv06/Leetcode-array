97870a3c49c0f4353f0a672c29867fc10b0421bc
naveenanv06 <naveenanv06@gmail.com>
2026-07-09 18:34:55 +0530

    Time: 4 ms (99.67%) | Memory: 47.6 MB (66.66%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    
+    public boolean ispali(String s, int l, int r) {
+
+        while (l < r) {
+
+            if (s.charAt(l) != s.charAt(r))
+                return false;
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+
+    public boolean validPalindrome(String s) {
+
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l < r) {
+
+            if (s.charAt(l) == s.charAt(r)) {
+
+                l++;
+                r--;
+
+            } else {
+
+                
+                return ispali(s, l+1, r) ||
+                       ispali(s, l, r - 1);
+            }
+        }
+
+        return true;
+    }
+}
