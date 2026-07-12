31f14f008cbab14a770f1647b8342d7782211a2f
naveenanv06 <naveenanv06@gmail.com>
2026-07-12 10:01:00 +0530

    Time: 1 ms (77.16%) | Memory: 42.7 MB (86.42%) - LeetSync

---
+++ 2154-minimum-moves-to-convert-string/minimum-moves-to-convert-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minimumMoves(String s) {
+
+        int count = 0;
+
+        for (int i = 0; i < s.length(); ) {
+
+            if (s.charAt(i) == 'X') {
+                count++;
+                i += 3;   
+            } else {
+                i++;
+            }
+        }
+
+        return count;
+    }
+}
