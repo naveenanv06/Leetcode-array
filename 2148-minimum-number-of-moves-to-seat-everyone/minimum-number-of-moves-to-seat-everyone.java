7bf3aa54086f0d082fdeaa7a4a4bc3430588f27a
naveenanv06 <naveenanv06@gmail.com>
2026-07-12 11:21:16 +0530

    Time: 6 ms (43.11%) | Memory: 46.8 MB (25.31%) - LeetSync

---
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        //return Math.abs(Arrays.stream(seats).sum()-Arrays.stream(students).sum());
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int sum=0;
+        for(int i=0;i<seats.length;i++){
+            sum+=Math.abs(seats[i]-students[i]);
+        }
+        return sum;
+    }
+}
