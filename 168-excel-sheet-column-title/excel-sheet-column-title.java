e8541ad5bfffe934717fedd863ad04ea27a10e84
naveenanv06 <naveenanv06@gmail.com>
2026-07-15 22:40:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (22.06%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String convertToTitle(int c) {
+
+        StringBuilder s = new StringBuilder();
+
+        while (c > 0) {
+
+            c--;
+
+            s.append((char) ('A' + (c % 26)));
+
+            c /= 26;
+        }
+
+        return s.reverse().toString();
+    }
+}
